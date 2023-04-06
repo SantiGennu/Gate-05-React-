49bbad495ab6848c4b85dcf0fc1d8a02958e6593
Santiago Gennuso <santigennu5@gmail.com>
2023-04-06 16:35:30 +0200

    comm

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Router from "./app/Router"
+import Router from "../src/app/router";
 
 function App() {
   return (
     <>
       <Router />
-
     </>
-  )
-
+  );
 }
 
-export default App
+export default App;
