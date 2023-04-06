4b08a2957eef12359113d20405c7347d2e3193e1
Santiago Gennuso <santigennu5@gmail.com>
2023-04-06 16:40:36 +0200

    changed image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,37 +1,43 @@
-import "./Hero.css"
+import "./Hero.css";
 
 const Hero = () => {
-    return (
-        <>
-            <div className='hero'>
-                <div className="hero-text">
-                    <div className="text-icon"> FLY CHEAPER <span className="symbol material-symbols-outlined">
-                        savings
-                    </span></div>
-                    <br /><br />
-                    <div className="text-icon">FLY FASTER <span className="symbol material-symbols-outlined">
-                        speed
-                    </span></div>
-                    <br /><br />
-                    <div className="text-icon"> FLY HAPPYER <span className="symbol material-symbols-outlined">
-                        sentiment_very_satisfied
-                    </span></div>
+  return (
+    <>
+      <div className="hero">
+        <div className="hero-text">
+          <div className="text-icon">
+            {" "}
+            FLY CHEAPER{" "}
+            <span className="symbol material-symbols-outlined">savings</span>
+          </div>
+          <br />
+          <br />
+          <div className="text-icon">
+            FLY FASTER{" "}
+            <span className="symbol material-symbols-outlined">speed</span>
+          </div>
+          <br />
+          <br />
+          <div className="text-icon">
+            {" "}
+            FLY HAPPYER{" "}
+            <span className="symbol material-symbols-outlined">
+              sentiment_very_satisfied
+            </span>
+          </div>
+        </div>
+        <div>
+          {" "}
+          <img className="logo-hero" src="/public/gate 05.png" alt="" />
+        </div>
 
+        <div className="hero-text1">
+          GATE 05 IS THE ONLY COMPANY WHO CARES ABOUT YOU.
+        </div>
+      </div>
+      <div className="divider"></div>
+    </>
+  );
+};
 
-                </div>
-                <div> <img className="logo-hero" src="/src/assets/gate 05.png" alt="" /></div>
-
-
-                <div className="hero-text1">
-                    GATE 05 IS THE ONLY COMPANY WHO CARES
-                    ABOUT YOU.
-
-                </div>
-            </div>
-            <div className="divider"></div>
-
-        </>
-    )
-}
-
-export default Hero
+export default Hero;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,40 @@
-import CartWidget from "./CartWidget"
-import "./NavBar.css"
-import { Container, Navbar, Nav } from "react-bootstrap"
-import { Link } from "react-router-dom"
-
-
+import CartWidget from "./CartWidget";
+import "./NavBar.css";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <>
-            <Navbar className="navBar" expand="lg">
-                <Container className="nav-container">
-                    <Link className="logo" to={"/"}> <img src="/src/assets/gate 05.png" alt="" width={140} height={100} /> </Link>
-                    <Navbar.Toggle className="ms-auto" aria-controls="basic-nnpavbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="nav-link mx-auto">
-                            <Link to={"/"}>Home</Link>
-                            <Link to={"/"}>Flights</Link>
-                            <Link to={"/"}>Offers</Link>
-                            <Link to={"/"}>Contact Us</Link>
-                        </Nav>
+  return (
+    <>
+      <Navbar className="navBar" expand="lg">
+        <Container className="nav-container">
+          <Link className="logo" to={"/"}>
+            {" "}
+            <img
+              src="/public/gate 05.png"
+              alt=""
+              width={140}
+              height={100}
+            />{" "}
+          </Link>
+          <Navbar.Toggle
+            className="ms-auto"
+            aria-controls="basic-nnpavbar-nav"
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="nav-link mx-auto">
+              <Link to={"/"}>Home</Link>
+              <Link to={"/"}>Flights</Link>
+              <Link to={"/"}>Offers</Link>
+              <Link to={"/"}>Contact Us</Link>
+            </Nav>
+          </Navbar.Collapse>
 
-                    </Navbar.Collapse>
+          <CartWidget />
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-                    <CartWidget />
-
-                </Container>
-            </Navbar>
-        </>
-    )
-}
-
-export default NavBar
+export default NavBar;
